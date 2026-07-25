65aac737ec338aeb3652f34122a3f1cb0322c5e5
yumf <yumf828@gmail.com>
2026-07-25 14:25:27 +0800

    Fix style

--- inflammation/models.py
+++ inflammation/models.py
@@ -10,7 +10,7 @@ and each column represents a single day across all patients.
 import numpy as np
 
 
-def load_csv(filename):  
+def load_csv(filename):
     """Load a Numpy array from a CSV
 
     :param filename: Filename of CSV to load
@@ -21,7 +21,7 @@ def load_csv(filename):
 def daily_mean(data):
     """Calculate the daily mean of a 2d inflammation data array.
 
-    :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
+    :param data: A 2D data array with inflammation data
     :returns: An array of mean values of measurements for each day.
     """
     return np.mean(data, axis=0)
@@ -30,7 +30,7 @@ def daily_mean(data):
 def daily_max(data):
     """Calculate the daily max of a 2d inflammation data array.
 
-    :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
+    :param data: A 2D data array with inflammation data
     :returns: An array of maximum values of measurements for each day.
     """
     return np.max(data, axis=0)
@@ -39,8 +39,7 @@ def daily_max(data):
 def daily_min(data):
     """Calculate the daily min of a 2d inflammation data array.
 
-    :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
+    :param data: A 2D data array with inflammation data
     :returns: An array of minimum values of measurements for each day.
     """
     return np.min(data, axis=0)
-
