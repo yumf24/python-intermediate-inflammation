c6586a15d04c57673806f2ff772de56e23897cbd
yumf <yumf828@gmail.com>
2026-07-26 14:24:38 +0800

    test(models): add more test cases for patient_normalise

--- tests/test_models.py
+++ tests/test_models.py
@@ -34,8 +34,12 @@ def test_daily_mean_integers():
 @pytest.mark.parametrize(
     "test, expected",
     [
-        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]])
-    ])
+        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]]),
+        ([[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]]),
+        ([[1, 1, 1], [1, 1, 1], [1, 1, 1]], [[1, 1, 1], [1, 1, 1], [1, 1, 1]]),
+        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]]),
+    ]
+)
 def test_patient_normalise(test, expected):
     """Test normalisation works for arrays of one and positive integers.
        Test with a relative and absolute tolerance of 0.01."""
