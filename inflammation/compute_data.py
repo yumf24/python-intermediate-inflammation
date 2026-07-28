1c61fb05127a3f3bbe666d8d1b486298ed312e73
yumf <yumf828@gmail.com>
2026-07-28 10:56:59 +0800

    feat(compute_data): add CSVDataSource class

--- inflammation/compute_data.py
+++ inflammation/compute_data.py
@@ -8,6 +8,18 @@ import numpy as np
 from inflammation import models, views
 
 
+class CSVDataSource():
+    def __init__(self, data_dir) -> None:
+        self.data_dir = data_dir
+
+    def load_inflamation_data(self):
+        data_file_paths = glob.glob(os.path.join(self.data_dir, 'inflammation*.csv'))
+        if len(data_file_paths) == 0:
+            raise ValueError(f"No inflammation data CSV files found in path {self.data_dir}")
+        data = map(models.load_csv, data_file_paths)
+        return list(data)
+
+
 def compute_standard_deviation_by_day(data):
     means_by_day = map(models.daily_mean, data)
     means_by_day_matrix = np.stack(list(means_by_day))
